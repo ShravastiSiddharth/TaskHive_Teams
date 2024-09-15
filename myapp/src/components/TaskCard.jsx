d3811434288efaad960324d32c6e94f15668e972
ShravastiSiddharth <91666088+ShravastiSiddharth@users.noreply.github.com>
2024-09-15 16:34:17 +0530

    Separated Expanded view Modal Component

--- myapp/src/components/TaskCard.jsx
+++ myapp/src/components/TaskCard.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import styles from '../styles/TaskCard.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faExpand, faTrash, faCalendarDays, faRectangleXmark, faHourglassHalf, faFlag,faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faExpand, faTrash, faCalendarDays, faRectangleXmark, faHourglassHalf, faFlag, faBookmark } from '@fortawesome/free-solid-svg-icons';
 import Modal from 'react-modal';
 import axios from 'axios';
 import { useAuth } from '../authentication/AuthContext';
 import { Draggable } from 'react-beautiful-dnd';
 import Swal from 'sweetalert2'
+import DescriptionModal from './DescriptionModal';
 
 const TaskCard = ({ task, onTaskMoved, onTaskUpdated, index, fetchTasks }) => {
     const { user } = useAuth();
@@ -40,7 +41,7 @@ const TaskCard = ({ task, onTaskMoved, onTaskUpdated, index, fetchTasks }) => {
             });
             setEditModalOpen(false);
             onTaskUpdated();
-            fetchTasks(); 
+            fetchTasks();
         } catch (error) {
             console.error('Error updating task:', error);
         }
@@ -143,75 +144,10 @@ const TaskCard = ({ task, onTaskMoved, onTaskUpdated, index, fetchTasks }) => {
                     </div>
 
 
-                    <Modal style={{border:'1px solid red'}} isOpen={isViewModalOpen} onRequestClose={() => setViewModalOpen(false)} className={styles.viewModal}>
-                        <div className={styles.viewCrossDiv}>
-                            <FontAwesomeIcon icon={faRectangleXmark} onClick={() => setViewModalOpen(false)} />
-
-                        </div>
-
-                        <div >
-                            <div className={styles.viewTitleDiv}>
-                                {/* <span>Title: </span> */}
-                                <span>{task.title}</span>
-                            </div>
-                            <div className={styles.viewDecDiv}>
-                                {/* <span>Description: </span> */}
-                                <span>{task.description}</span>
-
-                            </div>
-                            <div className={styles.viewTimeDiv}>
-
-                                <div className={styles.viewCrtDiv}>
-                                    <span style={{ color: 'blue',marginRight:'0.5rem' }}><FontAwesomeIcon icon={faBookmark} /></span>
-                                    <span> {formatDate(task.createdAt)} </span>
-                                    
-                                </div>
-
-                                <div className={styles.viewDeadinfo} >
-                                    <span style={{    color: '#2dcc2d',marginRight:'0.5rem'}}> <FontAwesomeIcon icon={faFlag} /> </span>
-                                    
-                                    <span>{formatDate(task.deadline)}</span>
-                                </div>
-
-                                <div className={styles.viewDeadinfo}>
-                                    <span style={{ color: '#ff7153',marginRight:'0.5rem'}}>
-                                    <FontAwesomeIcon icon={faHourglassHalf} />
-                                        </span>
-
-                                    <span >Only {daysBetweenDates(formatDate(task.createdAt), formatDate(task.deadline))} Days Left</span>
-                                </div>
-                               
-                            </div>
-
-                        </div>
-
-                        <div className={styles.viewExtraIcons}>
-                            {user && (
-                                
-                                <button className={`${styles.viewbtndiv} ${styles.viewEdit}`} onClick={() => setEditModalOpen(true)}><FontAwesomeIcon icon={faPenToSquare}  style={{ marginRight:'0.5rem' }} />Edit </button>
-
-                            )}
-
-                            <button className={`${styles.viewbtndiv} ${styles.viewDelete}` } onClick={() => {
-                                Swal.fire({
-                                    title: "Are you sure you want to delete this?",
-                                    showCancelButton: true,
-                                    confirmButtonText: "Delete",
-                                }).then((result) => {
-                                    if (result.isConfirmed) {
-                                        handleDelete();
-
-                                    } else if (result.isDenied) {
-                                        Swal.fire("Changes are not saved", "", "info");
-                                    }
-                                });
-                            }}>
-                            <FontAwesomeIcon icon={faTrash} style={{ marginRight:'0.5rem' }} />Delete</button>
-                        </div>
-
-                    </Modal>
+                   
 
 
+            <DescriptionModal isViewModalOpen={isViewModalOpen} setViewModalOpen={setViewModalOpen} description={task.description} title={task.title} createDate={formatDate(task.createdAt)} deadlineDate={formatDate(task.deadline)} RemainingDays={daysBetweenDates(formatDate(task.createdAt), formatDate(task.deadline))} handleDelete={handleDelete} user={user} setEditModalOpen={setEditModalOpen}/>
                     <Modal
                         isOpen={isEditModalOpen}
                         onRequestClose={() => setEditModalOpen(false)}
